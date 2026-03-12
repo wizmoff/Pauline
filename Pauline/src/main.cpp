593c95ccf58f7eb91266dd1335eb56c0c85888ba
Wiz <28dsthilaire@norwood.k12.ma.us>
2026-03-12 15:22:13 -0400

    001

--- Pauline/src/main.cpp
+++ Pauline/src/main.cpp
@@ -1,15 +1,49 @@
 #include <windows.h>
 #include <wrl/client.h>
-#include <iostream>
 #include "../include/pauline.h"
 
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
 
-int main() {
-    if (checkDXR())
-        std::cout << "DXR supported.";
-    else
-        std::cout << "DXR not supported.";
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+    if (msg == WM_DESTROY) {
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hwnd, msg, wp, lp);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
+    if (!checkDXR()) {
+        MessageBox(nullptr, L"DXR not supported.", L"Pauline", MB_OK);
+        return -1;
+    }
+
+    WNDCLASSEX wc = {};
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"PaulineWindow";
+    RegisterClassEx(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0,
+        L"PaulineWindow",
+        L"Pauline",
+        WS_OVERLAPPEDWINDOW,
+        100, 100, 1280, 720,
+        nullptr, nullptr, hInstance, nullptr
+    );
+
+    ShowWindow(hwnd, nCmdShow);
+
+    MSG msg = {};
+    while (msg.message != WM_QUIT) {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
     return 0;
 }
